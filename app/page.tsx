69f1249dfb8bf0255fa67a7f58f3d27e9835bd94
“Derek <dnielsen85@gmail.com>
2025-07-13 22:36:28 -0700

    another commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Features from "@/components/Features";
 import Footer from "@/components/Footer"
 import Video from "@/components/Video"
 import react from "react";
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
 
 
 
